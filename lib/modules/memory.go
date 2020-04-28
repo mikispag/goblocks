f8095adf14d1ecd0c36a0c8b887ed9fecda8f1a9
Michele Spagnuolo <mikispag@gmail.com>
2020-04-28 11:48:00 +0200

    Show memory as mem_used/mem_total.

--- lib/modules/memory.go
+++ lib/modules/memory.go
@@ -2,8 +2,9 @@ package modules
 
 import (
 	"fmt"
-	"github.com/davidscholberg/go-i3barjson"
 	"os"
+
+	"github.com/davidscholberg/go-i3barjson"
 )
 
 // Memory represents the configuration for the memory block.
@@ -13,11 +14,10 @@ type Memory struct {
 }
 
 // UpdateBlock updates the system memory block status.
-// The value dispayed is the amount of available memory.
 func (c Memory) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
-	var memAvail, memJunk int64
+	var memAvail, memFree, memTotal int64
 	r, err := os.Open("/proc/meminfo")
 	if err != nil {
 		b.Urgent = true
@@ -28,17 +28,19 @@ func (c Memory) UpdateBlock(b *i3barjson.Block) {
 	_, err = fmt.Fscanf(
 		r,
 		"MemTotal: %d kB\nMemFree: %d kB\nMemAvailable: %d ",
-		&memJunk, &memJunk, &memAvail)
+		&memTotal, &memFree, &memAvail)
 	if err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
 	}
-	memAvailG := float64(memAvail) / 1048576.0
+	memTotalG := float64(memTotal) / 1048576
+	memUsedG := float64(memTotal-memAvail) / 1048576
+	memAvailG := float64(memAvail) / 1048576
 	if memAvailG < c.CritMem {
 		b.Urgent = true
 	} else {
 		b.Urgent = false
 	}
-	b.FullText = fmt.Sprintf(fullTextFmt, fmt.Sprintf("%.2fG", memAvailG))
+	b.FullText = fmt.Sprintf(fullTextFmt, fmt.Sprintf("%.2fG / %.2fG", memUsedG, memTotalG))
 }
