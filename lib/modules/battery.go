9f788322e8fb2fca1e71221cce2cbda22f6ad92c
Michele Spagnuolo <mikispag@gmail.com>
2020-04-28 15:56:03 +0200

    Detect AC in battery.

--- lib/modules/battery.go
+++ lib/modules/battery.go
@@ -13,13 +13,37 @@ type Battery struct {
 	CritBattery     float64 `yaml:"crit_battery"`
 }
 
+const (
+	acFilePath = "/sys/class/power_supply/AC/online"
+)
+
 // UpdateBlock updates the battery status block.
 func (c Battery) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
+	r, err := os.Open(acFilePath)
+	if err != nil {
+		b.Urgent = true
+		b.FullText = err.Error()
+		return
+	}
+	defer r.Close()
+	var online int
+	_, err = fmt.Fscanf(r, "%d", &online)
+	if err != nil {
+		b.Urgent = true
+		b.FullText = err.Error()
+		return
+	}
+	if online == 1 {
+		c.Label = "🔌 "
+	} else {
+		c.Label = "🔋 "
+	}
+
 	fullTextFmt := fmt.Sprintf("%s%%d%%%%", c.Label)
 	var capacity int
-	sysFilePath := fmt.Sprintf("/sys/class/power_supply/BAT%d/capacity", c.BatteryNumber)
-	r, err := os.Open(sysFilePath)
+	batFilePath := fmt.Sprintf("/sys/class/power_supply/BAT%d/capacity", c.BatteryNumber)
+	r, err = os.Open(batFilePath)
 	if err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
